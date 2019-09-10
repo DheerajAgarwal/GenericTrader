5b15a7a0e8e46d7be1e5f4b4aa0059082977765a
Dheeraj Agarwal <dheeeraj.agarwal@gmail.com>
2019-09-10 19:08:01 +1000

    additional params

--- getdata.R
+++ getdata.R
@@ -1,5 +1,8 @@
+# Dependencies ----
 library(dplyr)
 library(httr)
+
+# Source parameters ----
 
 baseurl <- "https://www.alphavantage.co"
 
@@ -9,12 +12,16 @@ API_KEY <- "EUDLB81402IK8SIA"
 
 url <- paste0(baseurl, "/query?function=",func, "&symbol=",ticker,"&outputsize=full&apikey=",API_KEY)
 
-#url2 = "https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY&symbol=WOW.AX&outputsize=full&apikey=EUDLB81402IK8SIA"
-temp <- httr::GET(url) %>% content()
+# Fetch data ----
+
+temp <- httr::GET(url) %>%
+  content()
 
 meta_lastrefresh <- temp$`Meta Data`$`3. Last Refreshed`
 meta_comment <- temp$`Meta Data`$`1. Information`
 meta_refreshtz <- temp$`Meta Data`$`4. Time Zone`
+
+# Transform ----
 
 df <- do.call(rbind.data.frame, temp[[2]])
 df <- tibble::rownames_to_column(df, var = "Date")
@@ -22,6 +29,8 @@ df <- tibble::rownames_to_column(df, var = "Date")
 df["Ticker"] <- ticker
 
 df <- cleandf(df=df, source = baseurl, meta_lastrefresh, meta_refreshtz, meta_comment)
+
+# Memory clean up ----
 
 rm(baseurl, ticker, func, API_KEY, url, temp, meta_lastrefresh, meta_comment, meta_refreshtz)
 
